fee3ff143b5222dcd29d974b04e40d1e73b38b7d
Rumeysa <aydogdurumeysa99@hotmail.com>
2021-09-09 17:07:33 +0300

    sending mail with C# (MVC)

---
+++ SendMail/Controllers/MailController.cs
@@ -0,0 +1,52 @@
+﻿using SendMail.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
+
+namespace SendMail.Controllers
+{
+    public class MailController : Controller
+    {
+        // GET: Mail
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Mail mail)
+        {
+            MailMessage myMail = new MailMessage();
+            myMail.To.Add("rumcanva@gmail.com");  //liste türünde oludğu için To.Add()  kullanıyoruz
+            myMail.From = new MailAddress("rumeysaworks@gmail.com");  //mailimiz string ifade olduğu için, new MailAddress() kullanarak mail türünde bir değişken oluşturuyoruz
+            myMail.Subject = "MVC Mail'den: " + mail.Baslik;
+            myMail.Body = mail.Icerik;
+            myMail.IsBodyHtml = true;  //başlığa yazdığımız html etiketinin yorumlanabilmesi için
+
+            //aşağısı google gmail ayarları
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Credentials = new NetworkCredential("rumeysaworks@gmail.com", "mvcmail2");  //bu mesajı kimden gönderiyorsak onun mail ve şifresi
+            smtp.Port = 587;  //gmail ve hotmail 'in portu
+            smtp.Host = "smtp.gmail.com";   //hotmail için:  smtp.live.com
+            smtp.EnableSsl = true;   // https deki "S" için izin sanırım
+
+
+            try
+            {
+                smtp.Send(myMail);
+                TempData["Message"] = "Mesajınız başarılı bir şekilde gönderildi!";
+            }
+            catch(Exception ex)
+            {
+                TempData["Message"] = "Mesajınız gönderilemedi. Hata nedeni:" + ex.Message;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ SendMail/Models/Mail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SendMail.Models
+{
+    public class Mail
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Baslik { get; set; }
+        public string Icerik { get; set; }
+    }
+}
