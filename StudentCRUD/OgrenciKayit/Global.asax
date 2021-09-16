9e4709e74d37e7e66ac78e883830ffe64ba29ea3
Rumeysa <aydogdurumeysa99@hotmail.com>
2021-09-16 18:00:27 +0300

    CRUD operations with EF (code first) in MVC

---
+++ StudentCRUD/OgrenciKayit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OgrenciKayit.MvcApplication" Language="C#" %>
---
+++ StudentCRUD/OgrenciKayit/Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OgrenciKayit.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Ogrenci> Ogrencis { get; set; }
+    }
+}
